8d8810097fd7b8a1a5e57be186d354d25469aa03
mrq11920 <49233023+mrq11920@users.noreply.github.com>
2019-11-14 21:46:47 +0700

    add tower

--- src/mrmathami/thegame/entity/tile/tower/SniperTower.java
+++ src/mrmathami/thegame/entity/tile/tower/SniperTower.java
@@ -1,5 +1,18 @@
 package mrmathami.thegame.entity.tile.tower;
 
-public class SniperTower extends AbstractTower {
+import mrmathami.thegame.Config;
+import mrmathami.thegame.entity.bullet.SniperBullet;
 
+import javax.annotation.Nonnull;
+
+public class SniperTower extends AbstractTower<SniperBullet> {
+    public SniperTower(long createdTick, long posX, long posY) {
+        super(createdTick, posX, posY, Config.SNIPER_TOWER_RANGE, Config.SNIPER_TOWER_SPEED);
+    }
+
+    @Nonnull
+    @Override
+    protected final SniperBullet doSpawn(long createdTick, double posX, double posY, double deltaX, double deltaY) {
+        return new SniperBullet(createdTick, posX, posY, deltaX, deltaY);
+    }
 }
